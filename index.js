fc819c3c2099ecb208434af056215665c97adb0c
Alfredo Mujica <alfredo.mujica@underdog.mx>
2021-12-05 18:55:50 -0400

    test

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.use(cors({origin: '*'}));
 app.use( express.json({ extended: true }));
 app.use('', require('./routes/text'));
 
-
-
 // Start API
 const PORT = process.env.PORT || 4000;
 var server = app.listen(PORT, () => {
